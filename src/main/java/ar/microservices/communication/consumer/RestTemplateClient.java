486f62d8cafdfd6101b5d18ae8bf6d9b1aad3174
MartinMSalas <ingmartinmsalas@gmail.com>
2026-09-17 20:29:14 -0300

    adding files

---
+++ src/main/java/ar/microservices/communication/consumer/RestTemplateClient.java
@@ -0,0 +1,27 @@
+package ar.microservices.communication.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/*
+ * Author: m
+ * Date: 9/16/26
+ * Project Name: communication
+ * Description: beExcellent
+ */
+@Service
+@RequiredArgsConstructor
+public class RestTemplateClient {
+
+    private static final String PROVIDER_URL = "http://localhost:9091";
+
+
+    private final RestTemplate restTemplate;
+
+    public String getInstanceInfo() {
+
+        return restTemplate.getForObject(PROVIDER_URL + "/instance-info", String.class);
+    }
+}
---
+++ src/main/java/ar/microservices/communication/consumer/RestTemplateController.java
@@ -0,0 +1,29 @@
+package ar.microservices.communication.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/*
+ * Author: m
+ * Date: 9/16/26
+ * Project Name: communication
+ * Description: beExcellent
+ */
+@RestController
+@RequestMapping("/api/rest-template")
+@RequiredArgsConstructor
+public class RestTemplateController {
+
+    private final RestTemplateClient restClient;
+
+    @GetMapping("/instance")
+    public String getInstance(){
+
+        String response = restClient.getInstanceInfo();
+
+        return response;
+    }
+}
