27a3a50fe887f540d527150933cc7456baf4dc07
MartinMSalas <ingmartinmsalas@gmail.com>
2026-09-22 21:58:26 -0300

    feat<Feign>:Controller

---
+++ src/main/java/ar/microservices/communication/consumer/feign/FeignController.java
@@ -0,0 +1,28 @@
+package ar.microservices.communication.consumer.feign;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Author: m
+ * Date: 9/22/26
+ * Project Name: communication
+ * Description: beExcellent
+ */
+@RestController
+@RequestMapping("/api/feign")
+@RequiredArgsConstructor
+public class FeignController {
+
+    private final ProviderFeignClient providerFeignClient;
+
+    @GetMapping("/instance")
+    public String getInstance() {
+
+        return providerFeignClient.getInstanceInfo();
+    }
+
+
+}
